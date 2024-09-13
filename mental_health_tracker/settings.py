2fedec41ce6476e72eab03f4be84796a4e3a9628
abyasa05 <abyprat07@gmail.com>
2024-09-13 13:55:26 +0700

    fix URL

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://http://muhammad-abyasa-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://http://muhammad-abyasa-mentalhealthtracker.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://muhammad-abyasa-mentalhealthtracker.pbp.cs.ui.ac.id/", "https://muhammad-abyasa-mentalhealthtracker.pbp.cs.ui.ac.id/"]
